f0a7b1bb2dd7e8741de64d60a5e7aaeb8ff96f4a
iheqi <993284758@qq.com>
2026-09-24 14:09:30 +0800

    feat: use official BGE logo

--- src/Home.vue
+++ src/Home.vue
@@ -6,7 +6,7 @@ import { ArrowDown } from '@element-plus/icons-vue'
 <template>
   <div class="page">
     <header class="nav">
-      <div class="brand"><span class="brand-mark">╱</span><b>BGE</b></div>
+      <div class="brand"><img class="brand-logo" src="/logo.png" alt="" width="28" height="28" /><b>BGE</b></div>
       <nav class="main-links"><a class="active" href="/hk/zh-CN/">首页</a><a href="/hk/zh-CN/otc">场外</a><a href="#">现货交易</a><a class="company" href="/hk/zh-CN/about">公司 <el-icon><ArrowDown /></el-icon></a></nav>
       <div class="right-links"><a href="#">账户记录</a><a href="#">资产管理</a><MessageBell /><span class="avatar"><span></span></span><a href="#">简体中文</a><i></i><a href="#">USD</a></div>
     </header>
@@ -23,6 +23,6 @@ import { ArrowDown } from '@element-plus/icons-vue'
       <section class="about"><h2>进一步了解我们</h2><div class="about-row company-row"><div class="scene scene-hk"><div class="disc">✦</div><div class="disc coin-hk">◆</div></div><div class="copy"><h3>香港上市公司旗下成员</h3><p>BGE 为香港上市公司旗下成员公司，我们致力于打造一家安全、稳健、合规的数字资产交易平台，客户的资产安全和合规运营从第一天开始都是我们的首要任务。</p><strong><span class="hk-icon">✤</span> 股票代码 1726.HK</strong></div></div><div class="about-row safety-row"><div class="copy"><h3>资产安全</h3><ul><li>线下冷钱包存储 98% 的用户资产，冷温热三层安全结构</li><li>更高安全级别的 HSM 加密机</li><li>离线签名，多重验证</li><li>多级审核冷钱包体系确保资产安全</li></ul></div><div class="scene shield-scene"><div class="shield">✓</div><div class="coin green">₮</div><div class="coin gold">₿</div></div></div></section>
     </main>
 
-    <footer><div class="footer-inner"><a class="brand footer-brand" href="/hk/zh-CN/"><span class="brand-mark">╱</span><b>BGE</b></a><div><h4>公司</h4><a href="/hk/zh-CN/about">关于我们</a><a href="/hk/zh-CN/security">安全</a></div><div><h4>产品</h4><a href="#">现货交易</a><a href="/hk/zh-CN/otc">场外</a><h4 class="service">服务</h4><a href="#">帮助中心</a></div><div><h4>条款</h4><a href="#">条款及细则</a><a href="#">隐私政策</a><a href="#">免责声明</a><a href="#">交易规则</a><a href="#">Cookie政策</a><a href="#">Cookie偏好设置</a></div><div class="contact"><h4>关于我们</h4><a href="mailto:cs@bg.exchange">邮箱：cs@bg.exchange</a><a href="#">地址：香港上环干诺道中168-200号信德中心<br/>招商局大厦24楼2414-2416室</a></div></div><div class="legal"><span>BGE@2026</span><p>免责声明：HKBGF所提供的虚拟资产交易服务只针对专业投资者。虚拟资产的价值很可能剧烈波动。虚拟资产交易有极高风险，因此可能不适合所有投资者/交易者。虚拟资产过去的价格不代表未来的结果。 </p></div></footer>
+    <footer><div class="footer-inner"><a class="brand footer-brand" href="/hk/zh-CN/"><img class="brand-logo" src="/logo.png" alt="" width="28" height="28" /><b>BGE</b></a><div><h4>公司</h4><a href="/hk/zh-CN/about">关于我们</a><a href="/hk/zh-CN/security">安全</a></div><div><h4>产品</h4><a href="#">现货交易</a><a href="/hk/zh-CN/otc">场外</a><h4 class="service">服务</h4><a href="#">帮助中心</a></div><div><h4>条款</h4><a href="#">条款及细则</a><a href="#">隐私政策</a><a href="#">免责声明</a><a href="#">交易规则</a><a href="#">Cookie政策</a><a href="#">Cookie偏好设置</a></div><div class="contact"><h4>关于我们</h4><a href="mailto:cs@bg.exchange">邮箱：cs@bg.exchange</a><a href="#">地址：香港上环干诺道中168-200号信德中心<br/>招商局大厦24楼2414-2416室</a></div></div><div class="legal"><span>BGE@2026</span><p>免责声明：HKBGF所提供的虚拟资产交易服务只针对专业投资者。虚拟资产的价值很可能剧烈波动。虚拟资产交易有极高风险，因此可能不适合所有投资者/交易者。虚拟资产过去的价格不代表未来的结果。 </p></div></footer>
   </div>
 </template>
