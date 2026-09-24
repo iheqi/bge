57448f9da1e88b57bbf3c595ee0cbdb0ca984c89
iheqi <993284758@qq.com>
2026-09-24 18:09:35 +0800

    fix: keep spot page header navigation

--- src/Spot.vue
+++ src/Spot.vue
@@ -1,14 +1,58 @@
+<script setup>
+import LanguageSwitcher from './LanguageSwitcher.vue'
+import CurrencySwitcher from './CurrencySwitcher.vue'
+import MessageBell from './MessageBell.vue'
+import UserMenu from './UserMenu.vue'
+import { ArrowDown } from '@element-plus/icons-vue'
+</script>
+
 <template>
-  <main class="spot-placeholder">
-    <div
-      class="spot-placeholder-icon"
-      aria-hidden="true"
-    >
-      📈
-    </div>
-    <h1>{{ $t('text003') }}</h1>
-    <p>{{ $t('spotPlaceholder') }}</p>
-  </main>
+  <div class="spot-page">
+    <header class="otc-nav spot-nav">
+      <a
+        class="otc-brand"
+        href="/bge/hk/zh-CN/"
+        ><img
+          class="brand-logo brand-logo-full"
+          src="/logo-bge-light.svg"
+          alt="BGE"
+          width="116"
+          height="28"
+      /></a>
+      <nav>
+        <a href="/bge/hk/zh-CN/">{{ $t('text000') }}</a>
+        <a href="/bge/hk/zh-CN/market">{{ $t('text001') }}</a>
+        <a href="/bge/hk/zh-CN/otc">{{ $t('text002') }}</a>
+        <a
+          class="on"
+          href="/bge/hk/zh-CN/spot"
+          >{{ $t('text003') }}</a
+        >
+        <a href="/bge/hk/zh-CN/about"
+          >{{ $t('text004') }}<el-icon><ArrowDown /></el-icon
+        ></a>
+      </nav>
+      <div class="otc-right">
+        <a href="/bge/hk/zh-CN/user/report/spot">{{ $t('text005') }}</a
+        ><a href="/bge/hk/zh-CN/user/assets">{{ $t('text006') }}</a
+        ><MessageBell /><UserMenu /><LanguageSwitcher /><span
+          class="account-divider"
+          aria-hidden="true"
+        ></span
+        ><CurrencySwitcher />
+      </div>
+    </header>
+    <main class="spot-placeholder">
+      <div
+        class="spot-placeholder-icon"
+        aria-hidden="true"
+      >
+        📈
+      </div>
+      <h1>{{ $t('text003') }}</h1>
+      <p>{{ $t('spotPlaceholder') }}</p>
+    </main>
+  </div>
 </template>
 
 <style scoped>
